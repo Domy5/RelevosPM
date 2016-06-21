e4bdbd9b9f52e639be5cfc1d409b9dd570cf904d
Domy5 <domi.mtz@gmail.com>
2016-06-21 13:53:29 +0200

    arreglos de pintarcalendario view falta en PintarCalendario normal con meses

---
+++ app/src/main/java/domy/com/relevospm/AgenteDAO.java
@@ -0,0 +1,60 @@
+package domy.com.relevospm;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import domy.com.relevospm.Utiles.Utiles;
+
+
+public class AgenteDAO {
+
+    private JSONArray JsonTodo;
+    private Agente agente;
+
+    public Agente getAgente() {
+        return agente;
+    }
+
+    public boolean actualizarDatosAgente(String Dne, String Fecha) {
+
+        String F[] = Fecha.split("/");
+
+        String ordenSQL = "SELECT * FROM " + Utiles.periodo_Mes(F[0], F[1]) + "  WHERE `DNE` = " + Dne;
+        //   String ordenSQL = "SELECT * FROM " + a000_mes + "  WHERE `DNE` = " + "15910";
+
+        //  Log.v("Holaaaaaaaaaaaaaaaaaa","dddd");
+
+        JsonTodo = Diario.ConsultaDiaSQL(ordenSQL);
+
+        int x = 0;
+
+        try {
+
+            agente = new Agente(
+                    Integer.parseInt(JsonTodo.getJSONObject(x).getString("id")),
+                    Integer.parseInt(JsonTodo.getJSONObject(x).getString("N_MES")),
+                    JsonTodo.getJSONObject(x).getString("MES"),
+                    JsonTodo.getJSONObject(x).getString("PUESTO"),
+                    Integer.parseInt(JsonTodo.getJSONObject(x).getString("ORDEN")),
+                    Integer.parseInt(JsonTodo.getJSONObject(x).getString("ESCALAFON")),
+                    Integer.parseInt(JsonTodo.getJSONObject(x).getString("DNE")),
+                    JsonTodo.getJSONObject(x).getString("NOMBRE"),
+                    Integer.parseInt(JsonTodo.getJSONObject(x).getString("GRUPO")),
+                    JsonTodo.getJSONObject(x).getString("TURNO"),
+                    JsonTodo.getJSONObject(x).getString("VACACIONES_I"),
+                    JsonTodo.getJSONObject(x).getString("VACACIONES_T"),
+                    JsonTodo.getJSONObject(x).getString("VACACIONES"),
+                    Integer.parseInt(JsonTodo.getJSONObject(x).getString("CAMBIO_CON")),
+                    Integer.parseInt(JsonTodo.getJSONObject(x).getString("POR")),
+                    JsonTodo.getJSONObject(x).getString("COMPENSA"),
+                    JsonTodo.getJSONObject(x).getString("OBSERVACIONES"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+
+    }
+}
